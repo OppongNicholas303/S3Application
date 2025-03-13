82c3360e1d3fb4d45aa04d69804ca211834dbef0
nicholas <nicholas.oppong@amalitech.com>
2025-03-13 20:05:51 +0000

    using Instanceprofile

--- src/main/java/com/example/All/in/one/config/S3Config.java
+++ src/main/java/com/example/All/in/one/config/S3Config.java
@@ -14,19 +14,19 @@ public class S3Config {
     @Value("${aws.region}")
     private String awsRegion;
 
-//    @Bean
-//    public S3Client s3Client() {
-//        return S3Client.builder()
-//                .region(Region.of(awsRegion))
-//                .credentialsProvider(InstanceProfileCredentialsProvider.create())
-//                .build();
-//    }
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(ProfileCredentialsProvider.create()) // Uses ~/.aws/credentials
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .build();
     }
+
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(awsRegion))
+//                .credentialsProvider(ProfileCredentialsProvider.create()) // Uses ~/.aws/credentials
+//                .build();
+//    }
 }
